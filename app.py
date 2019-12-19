b236f35c4b239a89fb05f5ce50b7effab851d4f4
rickysmithson <rickysmithson160811@gmail.com>
2019-12-19 19:52:14 +0000

    intial commit updated

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-#from flask_pymongo import PyMongo
-#from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-#app.config["MONGO_DBNAME"] = 'web_jargon'
-#app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-7mwaw.mongodb.net/web_jargon?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'web_jargon'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-7mwaw.mongodb.net/web_jargon?retryWrites=true&w=majority'
 
-#mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/get_home')
+def get_home():
+    return render_template("home.html")
 @app.route('/get_defin')
 def get_defin():
-    return render_template("def.html")
-                           #tasks=mongo.db.definitions.find())
+    return render_template("def.html", 
+    defines=mongo.db.definitions.find(),
+    category=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
