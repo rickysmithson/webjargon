8afa26dcbe76b6d526a6fafd4ed65901c8c51fdd
rickysmithson <rickysmithson160811@gmail.com>
2019-12-20 11:06:10 +0000

    added definition page

--- app.py
+++ app.py
@@ -13,11 +13,24 @@ mongo = PyMongo(app)
 @app.route('/get_home')
 def get_home():
     return render_template("home.html")
+
 @app.route('/get_defin')
 def get_defin():
     return render_template("def.html", 
-    defines=mongo.db.definitions.find(),
+    defines=mongo.db.definitions.find())
+    
+
+@app.route('/add_jargon')
+def add_jargon():
+    return render_template('addjargon.html',
     category=mongo.db.categories.find())
+
+
+@app.route('/insert_jargon', methods=['POST'])
+def insert_jargon():
+    definitions =  mongo.db.definitions
+    definitions.insert_one(request.form.to_dict())
+    return redirect(url_for('add_jargon'))
 
 
 if __name__ == '__main__':
